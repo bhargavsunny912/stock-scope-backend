b3ed34935e80ead69e430f4c748896b873cb6d4c
Bhargav Penta <BhargavPenta@gmail.com>
2026-03-02 13:51:11 +0530

    Fixing OAuth Issues

--- Configs/passport.js
+++ Configs/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:"http://localhost:8000/auth/google/callback",
+      callbackURL:"https://stock-scope-backend.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
